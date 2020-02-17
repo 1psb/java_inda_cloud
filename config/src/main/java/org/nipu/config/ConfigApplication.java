3c172e246cecaf9391db93ef1e2e9f3db0797441
Pavel B <1psb>
2020-02-17 20:31:37 +0300

    added config server

---
+++ config/src/main/java/org/nipu/config/ConfigApplication.java
@@ -0,0 +1,14 @@
+package org.nipu.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(ConfigApplication.class, arguments);
+    }
+}
