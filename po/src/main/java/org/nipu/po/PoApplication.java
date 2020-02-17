e5ac7d37d532b4b63596b9fb19b69dc1c3604bee
Pavel B <1psb>
2020-02-17 20:45:23 +0300

    pc and po modules was prepared for config server usage

--- po/src/main/java/org/nipu/po/PoApplication.java
+++ po/src/main/java/org/nipu/po/PoApplication.java
@@ -4,7 +4,9 @@ import org.nipu.po.order.clients.ProductSpecificationRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 @EnableFeignClients(clients = ProductSpecificationRepository.class)
 public class PoApplication {
